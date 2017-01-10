eabcf7053485427235fcbbbf5be82ef23c037836
Michel Capitanio <kymy@MacBook-Pro-di-Michel.local>
2017-01-10 23:01:02 +0100

    added computer vision api and minor improvements in the webcam client

--- classes/webcamclient.py
+++ classes/webcamclient.py
@@ -10,6 +10,7 @@ from time import sleep
 import cv2
 from classes.emotionhttpservice import EmotionHttpService
 from classes.facehttpservice import FaceHttpService
+from classes.computervisionhttpservice import ComputerVisionService
 
 
 class WebcamClient():
@@ -19,13 +20,39 @@ class WebcamClient():
     logger = logging.getLogger('webcam_client')
     IMAGE_PATH = 'images/frame.jpg'
     _CASCADE_CLASSIFIER = 'data/haarcascade_frontalface_alt2.xml'
+    _local_face = True
+    _user_choices = {}
+    _is_emotion = False
+    _is_face = False
+    _is_cv = False
 
-    def __init__(self, emotion_api, face_api, handlers):
+    def __init__(self, emotion_api, face_api, computer_vision_api, handlers):
         self.emotion_client = EmotionHttpService(emotion_api)
         self.face_client = FaceHttpService(face_api)
+        self.cv_client = ComputerVisionService(computer_vision_api)
         self.handlers = handlers
         self.face_cascade = cv2.CascadeClassifier(self._CASCADE_CLASSIFIER)
         self._set_logger()
+
+    def disable_local_faces(self):
+        """ Disable OpenCV local face recognition"""
+        self._local_face = False
+
+    def enable_local_faces(self):
+        """ Enable OpenCV local face recognition"""
+        self._local_face = True
+
+    def set_emotion(self):
+        """ Enable the emotion detection"""
+        self._is_emotion = True
+
+    def set_face(self):
+        """ Enable the face detection """
+        self._is_face = True
+
+    def set_cv(self):
+        """ Enable the computer vision algorithm"""
+        self._is_cv = True
 
     def _detect_local_faces(self):
         """
@@ -33,9 +60,12 @@ class WebcamClient():
         algorithm
         """
         rects = []
+        if not self._local_face:
+            return [None], None
+
         image_temp = cv2.imread(self.IMAGE_PATH)
         gray = cv2.cvtColor(image_temp, cv2.COLOR_BGR2GRAY)
-        logging.info("Local faces detection...")
+        self.logger.info("Local faces detection...")
         faces = self.face_cascade.detectMultiScale(
             gray,
             scaleFactor=1.1,
@@ -49,9 +79,10 @@ class WebcamClient():
                 'w':width,
                 'h':height
             })
+        self.logger.info("Found {} local faces".format(len(faces)))
         return faces, rects
 
-    def run(self, user_choice):
+    def run(self):
         """
         Run the face/emotion detection by calling the Microsft Cognitive Services
         Arguments:
@@ -71,20 +102,25 @@ class WebcamClient():
             cv2.imwrite(self.IMAGE_PATH, frame)
 
             faces, rects = self._detect_local_faces()
-            self.logger.info("Found {} local faces".format(len(faces)))
 
-            if len(faces) != 0:
-                if user_choice == "1":
+            if self._is_emotion:
+                if len(faces) != 0:
                     self.emotion_client.get_emotion_async(
                         self.IMAGE_PATH,
                         self.handlers,
                         False,
                         rects)
-                else:
+            elif self._is_face:
+                if len(faces) != 0:
                     self.face_client.get_face_async(
                         self.IMAGE_PATH,
                         self.handlers,
-                        False
+                        False)
+            else:
+                self.cv_client.get_computer_vision_async(
+                    self.IMAGE_PATH,
+                    self.handlers,
+                    False
                     )
         video_capture.release()
 
--- client.py
+++ client.py
@@ -6,7 +6,6 @@ from pathlib import Path
 from classes.webcamclient import WebcamClient
 from classes.audioclient import AudioClient
 
-
 def on_success(res):
     """print thread success result"""
     print(res)
@@ -20,9 +19,20 @@ HANDLERS = {'on_success':on_success, 'on_failure': on_failure}
 def main(user_input):
     """ execute the ms emotion apis """
     try:
-        client = WebcamClient(os.environ['EMOTION_API'], os.environ['FACE_API'], HANDLERS)
-        user_input = "1" if user_input == 'emotion' else "2"
-        client.run(user_input)
+        client = WebcamClient(
+            os.environ['EMOTION_API'],
+            os.environ['FACE_API'],
+            os.environ['CV_API'],
+            HANDLERS)
+        if user_input == 'emotion':
+            client.set_emotion()
+        elif user_input == 'face':
+            client.set_face()
+        else:
+            client.set_cv()
+            client.disable_local_faces()
+
+        client.run()
     except KeyboardInterrupt:
         temp_image = Path(client.IMAGE_PATH)
         if temp_image.is_file():
@@ -33,10 +43,11 @@ if __name__ == '__main__':
 
     print("Say 'Emotion' if you want to detect the emotion")
     print("Say 'Face' if you want to detect the face")
+    print("Say 'Computer' if you want to run the computer vision detection")
 
     user_input = ""
     audioclient = AudioClient(os.environ['SPEECH_API'])
-    while user_input not in ["emotion", "face"]:
+    while user_input not in ["emotion", "face", "computer"]:
         print("I'm listening.... ")
         user_input = audioclient.run().lower()
         print("You're wrong: You said: {}".format("unrecognizable word" if user_input == 'error' else user_input))
